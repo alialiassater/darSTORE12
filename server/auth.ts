12e6e772f8591cdd3d05a3a7ceb09d1078200f01
alialiassater <122530267+alialiassater@users.noreply.github.com>
2026-02-07 11:44:48 +0100

    Update auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -33,7 +33,7 @@ export function setupAuth(app: Express) {
     cookie: {
       secure: isProduction,
       httpOnly: true,
-      sameSite: "lax" as const,
+      sameSite: "none" as const,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   };
